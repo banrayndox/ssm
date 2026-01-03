ec17965c601b576cac6e96b24f0bcb4b361ed27e
Your Name <your.email@example.com>
2026-01-04 02:38:55 +0600

    update all

--- src/helper/api.js
+++ src/helper/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', 
+  baseURL: 'https://ssm-backend-three.vercel.app/', 
   withCredentials: true,            
 });
 
